b3e82d861f9cb152218237c3ab6373fa2c648cf4
Jairo <jwu.monk@gmail.com>
2026-06-03 20:23:44 -0300

    Subiendo y completando tareas de python

---
+++ Estudios-UTN/TercerSemestre/Python_3_Smestre/Leccion07/capa_datos_persona/persona_dao.py
@@ -0,0 +1,93 @@
+# 8.4 Comienzo de la creación de la Clase PersonaDAO
+from capa_datos_persona.Persona import Persona
+from capa_datos_persona.conexion import Conexion
+from logger_base import log
+
+
+class PersonaDAO:
+    """
+    DAO significa: Data Access Object
+    CRUD significa:
+                    Create  -> Insertar
+                    Reade   -> Seleccionar
+                    Update  -> Actualizar
+                    Deleted -> Eliminar
+    """
+    _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
+    _INSERTAR = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
+
+    # Clase 08
+    #9.1 En la clase PersonaDao: metodo seleccionar
+    # Definimos los metodos de clase
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas = []  # Creamos una lista
+                for registro in registros:
+                    persona = Persona(
+                        id_persona=registro[0],
+                        nombre=registro[1],
+                        apellido=registro[2],
+                        email=registro[3]
+                    )
+                    personas.append(persona)
+                return personas
+
+    # 9.3 Metodo Insertar
+
+    @classmethod
+    def insertar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email)
+                cursor.execute(cls._INSERTAR, valores)
+                log.debug(f'Persona Insertada: {persona}')
+                return cursor.rowcount
+
+    # 9.4 Metodo Actualizar
+    @classmethod
+    def actualizar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email, persona.id_persona)
+                cursor.execute(cls._ACTUALIZAR, valores)
+                log.debug(f'Persona actualizada: {persona}')
+                return cursor.rowcount
+
+#
+    @classmethod
+    def eliminar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.id_persona,)
+                cursor.execute(cls._ELIMINAR, valores)
+                log.debug(f'Los objetos eliminados son: {persona}')
+                return cursor.rowcount
+
+# 9.2 Prueba metodo seleccionar
+
+if __name__ == '__main__':
+
+    # Eliminiar un registro
+    persona1 = Persona(id_persona=23)
+    personas_eliminadas = PersonaDAO.eliminar(persona1)
+    log.debug(f'Personas eliminadas: {personas_eliminadas}')
+    # Actualizar un registro
+    #persona1 = Persona(1, 'Juan Jose', 'Pena', 'jjpena@gmail.com')
+    #personas_actualizadas = PersonaDAO.actualizar(persona1)
+    #log.debug(f'Personas actualizadas: {personas_actualizadas}')
+
+    # Insertar un registro
+    #persona1 = Persona(nombre='Omero', apellido='Ramos', email='omeror@gmail.com')
+    #personas_insertadas = PersonaDAO.insertar(persona1)
+    #log.debug(f'Personas insertadas: {personas_insertadas}')
+
+    # Seleccionar objetos
+    personas = PersonaDAO.seleccionar()
+    for persona in personas:
+        log.debug(persona)
