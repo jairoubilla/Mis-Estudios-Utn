a857eff06a81410e28e062f192df6b49f7ed6944
Jairo <jwu.monk@gmail.com>
2026-08-17 19:44:07 -0300

    Agregando las primeras clases de js

---
+++ Estudios-UTN/Cuarto-Semestre/JavaScript/e-comerce/client/js/products.js
@@ -0,0 +1,41 @@
+const productos = [
+    {
+        id: 1,
+        productName: "locion",
+        price: 18.000,
+        quanty: 14,
+        img: "/client/media/locion_ek.jpg"
+    },
+
+    {
+        id: 2,
+        productName: "locion",
+        price: 16.000,
+        quanty: 12,
+        img: "/client/media/locion_la_vida_es_bella.jpg"
+    },
+
+    {
+        id: 3,
+        productName: "locion",
+        price: 11.000,
+        quanty: 11,
+        img: "/client/media/locion_luxury.jpg"
+    },
+
+    {
+        id: 4,
+        productName: "locion",
+        price: 18.400,
+        quanty: 15,
+        img: "/client/media/locion_olympia.jpg"
+    },
+
+    {
+        id: 5,
+        productName: "locion",
+        price: 17.600,
+        quanty: 12,
+        img: "/client/media/locion_polo.jpg"
+    }
+]
