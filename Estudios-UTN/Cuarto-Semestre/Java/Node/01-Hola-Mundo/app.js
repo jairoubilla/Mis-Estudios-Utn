e9e4bba9024e8bda31f2af7f6827bfb463e2182d
Jairo <jwu.monk@gmail.com>
2026-08-29 20:09:35 -0300

    Subiendo las primeras tres clases de node

---
+++ Estudios-UTN/Cuarto-Semestre/Java/Node/01-Hola-Mundo/app.js
@@ -0,0 +1,11 @@
+// Clase 1 - Hola mundo desde node
+const nombre = "Jairo";
+console.log(nombre);
+
+const nombre2 = "Juan";
+console.log(nombre2);
+
+// Clase 2 - Hola mundo desde node
+console.log("Hola mundo desde node");
+
+
---
+++ Estudios-UTN/Cuarto-Semestre/Java/Node/01-Hola-Mundo/app2.js
@@ -0,0 +1,5 @@
+//Clase 3 Comenzamos con pruebas en ejecución
+const saludar = (nombre) => { //Funcion flecha
+    return `Saludos de ${nombre}`; //Usmos back tic`s
+} //Esto es igual a: 'saludos ' + nombre;
+console.log(saludar('Jairo'));
